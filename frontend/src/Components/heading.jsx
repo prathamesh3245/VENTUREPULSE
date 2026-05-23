43ed7606184d723e0a2a34ac633c5a27c895dadc
Prathmesh <126573912+prathamesh3245@users.noreply.github.com>
2026-05-23 19:06:28 +0530

    final code

--- frontend/src/Components/heading.jsx
+++ frontend/src/Components/heading.jsx
@@ -6,11 +6,11 @@ export const Heading = () => {
             <center>
                 <div className="heading">
                     <p>VENTUREPULSE turns builders</p>
-                    <p>into <i>formidable founders[1]</i></p>
+                    <p>into <i>formidable founders</i></p>
                 </div>
                 <div className="thought">
                     <p>
-                        [1] “A formidable person is one who seems like they’ll get what they want, regardless of whatever obstacles are in the way.”
+                         “A formidable person is one who seems like they’ll get what they want, regardless of whatever obstacles are in the way.”
                     </p>
                     <p>- Paul Graham</p>
                 </div>
